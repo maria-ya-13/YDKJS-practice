12a43d60aeb4bb7223507dd7e84413babf783ade
orca <mariya.yakimovich@digexco.com>
2021-04-18 16:05:01 +0300

    Added using Symbol.iterator

--- closure_2.js
+++ closure_2.js
@@ -1,8 +1,20 @@
 //You will pass one or more values (as arguments) into toggle(..), and get back a function.
 // That returned function will alternate/rotate between all the passed-in values in order, one at a time, as it's called repeatedly.
 
-function toggle(/* .. */) {
-    // ..
+function toggle(input_array) {
+    var storage = [];
+    storage.push(input_array);
+    var iter = storage[Symbol.iterator]();
+    // let result = iter.next();
+    // console.log(result.value);
+
+    return function get_next() {
+        // iter = storage[Symbol.iterator]();
+        let result = iter.next();
+        // iter = storage[++Symbol.iterator]()
+        console.log(result.value);
+
+     }
 }
 
 var hello = toggle("hello");
